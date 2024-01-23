ebc420356e5a07b350faaef164cce7485bfca7b3
Patric Eckhart <mail@patriceckhart.com>
2024-01-23 17:06:07 +0100

    copy passwd and shadow temporarly

--- root-files/init.sh
+++ root-files/init.sh
@@ -7,6 +7,8 @@ chmod +x /usr/local/bin/user
 rm /etc/ssh/sshd_config
 cp /root-files/opt/sshd_config /etc/ssh/sshd_config
 chmod -rw-r--r-- /usr/local/bin/user
+yes | cp -rf /home/passwd /etc/passwd
+yes | cp -rf /home/shadow /etc/shadow
 
 echo "Starting ssh service ..."
 service ssh start
--- root-files/opt/user.sh
+++ root-files/opt/user.sh
@@ -6,6 +6,8 @@ create() {
   groupadd g"$current_timestamp"
   usermod -g g"$current_timestamp" u"$current_timestamp"
   chmod -R 775 /home/u"$current_timestamp"
+  yes | cp -rf /etc/passwd /home/passwd
+  yes | cp -rf /etc/shadow /home/shadow
 }
 
 if [ "$1" == "create" ]; then
